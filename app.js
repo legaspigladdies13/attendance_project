ee96de5ee72cef8b3c18ea643e511637937133b7
legaspigladdies6@gmail.com <legaspigladdies6@gmail.com>
2024-08-22 10:17:31 -0700

    comment

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-const express = require('express'); 
+const express = require ('express');
 const mongoose = require('mongoose');
 const app = express();
-require('dotenv').config(); 
-const PORT = process.env.PORT || 3000;
+require ('dotenv').config();
+const authRoutes = require('./routes/authRoutes.js');
+const studentRoutes = require ('./routes/studentRoutes.js');
+const PORT = process.env.PORT ||  3000;
 
-//Connect to MongoDB
+//connect to mongoDB
 mongoose.connect(process.env.MONGO_URI)
-.then(() => { console.log('Connected to MongoDB Database') })
-.catch(err => { console.log(`Error connecting to database: ${err}`) });
+.then(() =>{
+    console.log('Connected to MongoDB Database')
+})
+.catch(err =>{
+    console.log(`Error connecting to database: ${err}`)
+});
 
-
-//View engines
+//view engines
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-//Middlewares
+//middlewares
 app.use(express.static('public'));
+app.use('/', authRoutes);
+app.use('/', studentRoutes);
 
-//Start server
-app.listen(PORT,(connected) =>{
+
+
+app.use((err, res, req, next) =>{
+    console.error(err.stack);
+    res.status(500).send('Internal Server Error');
+    next();
+});
+
+
+//start server
+app.listen(PORT, () =>{
     console.log(`Connected to port ${PORT}`);
 });
